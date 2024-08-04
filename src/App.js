d13a7ce8619a77e9a628bffdfeedbe2bdfd7438e
snehal patil <patilonlysnehal2003@gmail.com>
2024-08-04 23:30:48 +0530

    Initial commit with authentication done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useAuth0 } from "@auth0/auth0-react";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const { user, loginWithRedirect, isAuthenticated, logout } = useAuth0();
+  console.log("Current user", user);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isAuthenticated && <h3>Hello {user.given_name}</h3>}
+        {isAuthenticated ? (
+          <button onClick={(e) => logout()}>Logout</button>
+        ) : (
+          <button onClick={(e) => loginWithRedirect()}>
+            Login with redirect
+          </button>
+        )}
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider 
+      domain="dev-mw1wsoj060b83ffb.us.auth0.com"
+    clientId="KpjlE2Iy864WFwbTGsQFMrq32vkqc14y"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
